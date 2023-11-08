9ef6bc5d7aa97d5f60d6f9c91b3459dd90ed0878
csederman <crs7240@gmail.com>
2023-11-08 16:44:31 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,14 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-requirements = []
+requirements = [
+    "numpy >= 1.21",
+    "pandas >= 2.0.3",
+    "tensorflow",
+    "tensorflow-probability",
+    "scikit-learn >= 1.3.0",
+    "h5py >= 3.9.0",
+]
 
 with open("./test-requirements.txt") as test_reqs_txt:
     test_requirements = [line for line in test_reqs_txt]
@@ -17,10 +24,10 @@ with codecs.open("./README.md", encoding="utf-8") as readme_md:
 setup(
     name="cdrpy",
     use_scm_version={"write_to": "cdrpy/_version.py"},
-    description="Cancer drug response prediction tools.",
+    description="Cancer drug response prediction toolkit.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="#",
+    url="https://github.com/csederman/cdrpy",
     packages=find_packages(exclude=["tests.*", "tests"]),
     setup_requires=["setuptools_scm"],
     install_requires=requirements,
@@ -34,6 +41,10 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     maintainer="Casey Sederman",
     maintainer_email="casey.sederman@hsc.utah.edu",
