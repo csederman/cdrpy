b0008eb26e5caec872e1fd1468971482486c110c
csederman <crs7240@gmail.com>
2024-01-17 08:38:55 -0700

    Register custom metrics as keras serializeable

--- cdrpy/metrics/tf_metrics.py
+++ cdrpy/metrics/tf_metrics.py
@@ -8,7 +8,10 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 import keras.backend as K
 
+from tensorflow import keras
 
+
+@keras.utils.register_keras_serializable(package="cdrpy")
 def r2(y_true: tf.Tensor, y_pred: tf.Tensor) -> float:
     """Calculates r-squared."""
     ss_res = K.sum(K.square(y_true - y_pred))
@@ -16,11 +19,13 @@ def r2(y_true: tf.Tensor, y_pred: tf.Tensor) -> float:
     return 1 - (ss_res / ss_tot)
 
 
+@keras.utils.register_keras_serializable(package="cdrpy")
 def pearson(y_true: tf.Tensor, y_pred: tf.Tensor) -> float:
     """Calculates pearson correlation."""
     return tfp.stats.correlation(y_true, y_pred)
 
 
+@keras.utils.register_keras_serializable(package="cdrpy")
 def rmse(s_true, s_pred):
     """Calculates root mean squared error."""
     return K.sqrt(K.mean(K.pow(s_true - s_pred, 2)))
