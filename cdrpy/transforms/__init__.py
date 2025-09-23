baad62c38a1cd215c31d1dfb513e3ee061ab606e
csederman <crs7240@gmail.com>
2025-09-23 12:04:48 -0600

    Added smiles to desc

--- cdrpy/transforms/__init__.py
+++ cdrpy/transforms/__init__.py
@@ -3,5 +3,6 @@ from .select_features import SelectCellFeatures
 from .select_features import SelectDrugFeatures
 from .select_features import SelectGenes
 from .smiles_to_morgan import SmilesToMorgan
+from .smiles_to_descriptors import SmilesToDescriptors
 from .variance_filter import CellFeatureVarianceFilter
 from .variance_filter import DrugFeatureVarianceFilter
